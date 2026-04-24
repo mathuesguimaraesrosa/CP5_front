6542627ecdc52e1889b0dc51b56275b6812bc643
pedrofalchi-fullstack <peeh.falchi@gmail.com>
2026-04-24 16:35:42 -0300

    Atualização de Banner.tsx com fade e zoom no className de <img>

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -11,6 +11,7 @@ export default function Banner() {
   ];
 
   const [index, setIndex] = useState(0);
+  const [fade, setFade] = useState(true);
 
   const proximo = () => {
     setIndex((prev) => (prev + 1) % imagens.length);
@@ -22,18 +23,22 @@ export default function Banner() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      proximo();
+      setFade(false);
+      setTimeout(() => {
+      setIndex((prev) => (prev + 1) % imagens.length);
+      setFade(true);
+    }, 300);
     }, 3000); // 3 segundos
 
-    return () => clearInterval(interval); // limpa quando muda
-  }, []);
+    return () => clearInterval(interval);
+}, [imagens.length]);
 
   return (
     <div className="relative w-full h-[70vh] overflow-hidden rounded-2xl shadow-xl">
       
       <img
         src={imagens[index]}
-        className="w-full h-full object-cover"
+        className={`w-full h-full object-cover transition-opacity duration-500 ${fade ? "opacity-100 scale-110" : "opacity-0 scale-110"}`}
         alt="Banner"
       />
 
